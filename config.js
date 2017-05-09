d7c0c406fe49c3c25e23bde81982df6f7e2733fc
Luis.O.M <luismunoz.dh@gmail.com>
2017-05-09 19:28:50 -0300

    config db

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 	'port' : 8080,
 	'tokenExpires' : 86400, // In seconds - 86400 = 24 hours ,
 	'db' : {
-		'name' : 'mp_admin_db',
+		'name' : 'mp_admin',
 		'host' : '127.0.0.1',
 		'user' : 'root',
 		'password' : 'root1234'
